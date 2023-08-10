094061c9e96d926eacd03b0bda2a06766c917ad3
Turgynbekov Nurbolat <turgynbekov.nurbolat.nurlanovich@gmail.com>
2023-08-10 14:52:50 +0600

    version-1.1.7

--- src/shared/ui/checkbox.tsx
+++ src/shared/ui/checkbox.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 //import { Checkbox } from '@mantine/core';
 
-export function UiCheckBox() {
+export async function UiCheckBox() {
   return (<input type='number' defaultValue={12}/>);
 }
